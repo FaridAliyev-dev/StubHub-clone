2b2a56777cd7683db08142f21a7bebb468603ebe
FaridAliyev-dev <farid.aliyevv258@gmail.com>
2025-01-27 12:19:00 +0400

    patch notes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const cardContainer = document.querySelector(".AllEventCardsContainer");
+const AllEventCardContainer = document.querySelector(".AllEventCardsContainer");
 
 fetch("http://localhost:3000/events")
   .then((response) => response.json())
@@ -20,6 +20,34 @@ fetch("http://localhost:3000/events")
       `;
     });
 
-    cardContainer.innerHTML = cardsHTML;
+    AllEventCardContainer.innerHTML = cardsHTML;
+  })
+  .catch((error) => console.error("Error fetching events:", error));
+
+
+  // TICKET CARDS
+const avaliableTickets = document.querySelector(".TicketCardsContainer");
+
+fetch("http://localhost:3000/tickets")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    const events = data;
+    let cardsHTML = "";
+
+    events.forEach((event) => {
+      cardsHTML += `
+        <div class="card">
+          <img src="${event.posterURL}" alt="${event.name}" />
+          <div class="cardContent">
+            <h3>${event.name}</h3>
+            <p>${new Date(event.dateTime).toLocaleString()}</p>
+            <p>${event.venueName}</p>
+          </div>
+        </div>
+      `;
+    });
+
+    avaliableTickets.innerHTML = cardsHTML;
   })
   .catch((error) => console.error("Error fetching events:", error));
