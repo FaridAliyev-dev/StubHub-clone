52c119c7fb69ba81447bd3b925d22c48d4ee0c9e
FaridAliyev-dev <farid.aliyevv258@gmail.com>
2025-02-01 00:03:35 +0400

    Add profile page and update user profile picture URLs in data.json

--- src/main.js
+++ src/main.js
@@ -24,8 +24,7 @@ fetch("http://localhost:3000/events")
   })
   .catch((error) => console.error("Error fetching events:", error));
 
-
-  // TICKET CARDS
+// TICKET CARDS
 const avaliableTickets = document.querySelector(".TicketCardsContainer");
 
 fetch("http://localhost:3000/tickets")
@@ -52,8 +51,33 @@ fetch("http://localhost:3000/tickets")
   })
   .catch((error) => console.error("Error fetching events:", error));
 
+const nav = document.querySelector(".nav-menu");
+if (localStorage.getItem("user")) {
+  const user = localStorage.getItem("user");
+  userData = JSON.parse(user);
+  console.log(user);
 
-  
+  nav.innerHTML = `
+            <ul class="nav-list">
+            <li><a href="#">Festivals</a></li>
+            <li><a href="#">Favorites</a></li>
+            <li><a href="#">My Tickets</a></li>
+            <li style="display: flex; flex-direction: row; ">
+            <a href="profile.html" class="sign-in" style="display: flex; align-items: center; width: 100%; gap: 10px">
+            
+            <div style="width: 30px; height: 30px;">
+            <img src="${userData.profilePictureURL}" alt="profile picture" style="width: 100%;  height: 100%; object-fit: cover; border-radius: 50%; margin-right: 10px">
+            </div>
 
+            ${userData.username}</a></li>
+            <li><a href="login.html" class="log-out">Log Out</a></li>
+          </ul>
+  `;
 
-  
+  document.querySelector(".log-out").addEventListener("click", () => {
+    localStorage.clear();
+  });
+  console.log("User is logged in");
+} else {
+  console.log("Salam");
+}
